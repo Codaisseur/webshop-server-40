bea9bb3d84c0bea7c151a2ae66bf855ed5fcb2de
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-11 19:06:59 +0200

    add try/catch

--- routers/order.js
+++ routers/order.js
@@ -10,32 +10,34 @@ router.post("/", async (req, res, next) => {
   if (!userId || !productIds || !productIds.length) {
     res.status(400).send("missing parameters");
   } else {
-    // this part is interesting.
-    // we have the id of the user and some productIds.
-    // we need to 1st create the Order so we can use the new id
-    // when inserting in orderProducts
+    try {
+      // this part is interesting.
+      // we have the id of the user and some productIds.
+      // we need to 1st create the Order so we can use the new id
+      // when inserting in orderProducts
 
-    const newOrder = await Order.create({ date: "thisShouldBeADate" });
+      const newOrder = await Order.create({ date: "thisShouldBeADate" });
 
-    // we map the array of product Ids into 'creates'. But doing this we are not awaiting
-    // correctly. We are going to get back an array of promises.
-    const orderProductCreatePromises = productIds.map(
-      async pId =>
-        await OrderProducts.create({ orderId: newOrder.id, productId: pId })
-    );
+      // we map the array of product Ids into 'creates'. But doing this we are not awaiting
+      // correctly. We are going to get back an array of promises.
+      const orderProductCreatePromises = productIds.map(
+        async pId =>
+          await OrderProducts.create({ orderId: newOrder.id, productId: pId })
+      );
 
-    //Using this we await on the whole array of promises as if it was just one
-    await Promise.all(orderProductCreatePromises);
+      //Using this we await on the whole array of promises as if it was just one
+      await Promise.all(orderProductCreatePromises);
 
-    //we now get again the order but with the products to check
-    const orderWithProducts = await Order.findByPk(newOrder.id, {
-      include: [Product, User],
-    });
+      //we now get again the order but with the products to check
+      const orderWithProducts = await Order.findByPk(newOrder.id, {
+        include: [Product, User],
+      });
 
-    res.send(orderWithProducts);
+      res.send(orderWithProducts);
+    } catch (e) {
+      next(e);
+    }
   }
-  try {
-  } catch (e) {}
 });
 
 router.get("/", async (req, res, next) => {
