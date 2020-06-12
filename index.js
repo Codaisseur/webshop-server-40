f26d735b82c8df80398f47755b00647ff88d3ed7
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-12 17:10:36 +0200

    add cors

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const authRouter = require("./routers/auth");
 const orderRouter = require("./routers/order");
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
